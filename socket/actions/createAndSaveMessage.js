dc205352b84bce5d88af8c29919c70b557cd9671
Levion6611 <tony6611brown@gmail.com>
2026-02-07 01:25:08 +0100

    Make new change on app

--- socket/actions/createAndSaveMessage.js
+++ socket/actions/createAndSaveMessage.js
@@ -2,17 +2,22 @@
 import Message from "../../models/Message.js";
 
 export async function createAndSaveMessage(payload, senderId) {
-  const message = new Message({
-    messageId: payload.messageId || `msg_${Date.now()}_${Math.round(Math.random() * 9999)}`,
-    temporaryId: payload.temporaryId || null,
-    senderId: senderId,
-    receiverId: payload.receiverId,
-    text: payload.text || '',
-    media: payload.media || [],
-    file: payload.file || [],
-    type: payload.type || 1
-  });
-
-  const saved = await message.save();
-  return saved;
+  try {
+    const message = new Message({
+      messageId: payload.messageId || `msg_${Date.now()}_${Math.round(Math.random() * 9999)}`,
+      temporaryId: payload.temporaryId || null,
+      senderId: senderId,
+      receiverId: payload.receiverId,
+      text: payload.text || '',
+      media: payload.media || [],
+      file: payload.file || [],
+      type: payload.type || 1
+    });
+  
+    const saved = await message.save();
+    return saved;
+  } catch(error) {
+    console.error("❌ Erreur lors de la sauvegarde du message:", error);
+    throw error;
+  }
 }
